2986c79ee14e6cd68a337072e0103793469d6b44
CChen-Z <208695557+CChen-Z@users.noreply.github.com>
2025-07-25 21:21:54 +0100

    create a UI with DOM and events.

--- javascript.js
+++ javascript.js
@@ -18,53 +18,63 @@ function getHumanChoice(){
 function playRound(humanChoice,computerChoice){
     if (humanChoice==computerChoice){
         console.log('Even!');
+        outcome.textContent='Even!';
         return;
     }
     if (humanChoice=='rock'){
         switch (computerChoice){
             case 'paper': 
                 console.log('You lose!');
+                outcome.textContent='You lose!';
                 computerScore ++;
                 break;
             default: 
                 console.log('You win!');
+                outcome.textContent='You win!';
                 humanScore ++;
         } 
     }else if (humanChoice=='paper'){
         switch (computerChoice){
             case 'scissors': 
                 console.log('You lose!');
+                outcome.textContent='You lose!';
                 computerScore ++;
                 break;
             default: 
                 console.log('You win!');
+                outcome.textContent='You win!';
                 humanScore ++;
         } 
     }else if (humanChoice=='scissors'){
         switch (computerChoice){
             case 'rock': 
                 console.log('You lose!');
+                outcome.textContent='You lose!';
                 computerScore ++;
                 break;
             default: 
                 console.log('You win!');
+                outcome.textContent='You win!';
                 humanScore ++;
         } 
     }else {console.log('Invalid Input')}
 }
 
-function playGame(){
-    for (let i=0; i<5; i++){
-        let computerChoice=getComputerChoice();
-        let humanChoice = getHumanChoice();
-        playRound(humanChoice,computerChoice);
-        console.log('Computer:',computerChoice,'You:',humanChoice,'\nComputer score:',computerScore,'Your score:',humanScore);
-    }
+function playGame(event){
+    let computerChoice=getComputerChoice();
+    let humanChoice = event.target.textContent.toLowerCase();
+    playRound(humanChoice,computerChoice);
+
+    console.log('Computer:',computerChoice,'You:',humanChoice,'\nComputer score:',computerScore,'Your score:',humanScore);
+    computer.textContent=`Computer:${computerChoice},\u00A0\u00A0Score:${computerScore}`;
+    human.textContent=`Human:${humanChoice},\u00A0\u00A0Score:${humanScore}`;
+    
 }
 
 let humanScore = 0;
 let computerScore = 0;
-playGame();
+
+
 
 
 
